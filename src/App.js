6abb20d6bee8f3a66071508f4ae62e587f4ec8f1
Thom van Dijk <thomvandijk@tutanota.com>
2021-05-11 10:44:37 +0200

    Added ARCanvas with a simple Box

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { ARCanvas } from '@react-three/xr'
+import { Box } from '@react-three/drei'
+
 function App() {
   return (
-    null
+    <ARCanvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[0, 0, -1]} args={[0.1, 0.1, 0.1]}/>      
+    </ARCanvas>
   );
 }
 
