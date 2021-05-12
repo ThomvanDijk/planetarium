bc2e515c3a49ba7ce9c3ce477eda49851e0e7eda
Thom van Dijk <thomvandijk@tutanota.com>
2021-05-12 10:49:45 +0200

    Added moving clouds to earth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Canvas } from '@react-three/fiber'
-import { Suspense } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Suspense, useRef } from 'react'
 // import { ARCanvas } from '@react-three/xr'
 import { OrbitControls, useTexture, Sphere } from '@react-three/drei'
 
 import moon_texture from './assets/textures/moon.png'
-import earth_texture from './assets/textures/earth.jpeg'
+import earth_texture from './assets/textures/earth.jpg'
+import earth_clouds_texture from './assets/textures/earth_clouds.jpg'
 
 function Moon(props) {
   const texture = useTexture(moon_texture)
@@ -22,13 +23,23 @@ function Moon(props) {
 
 function Earth() {
   const texture = useTexture(earth_texture)
+  const clouds_texture = useTexture(earth_clouds_texture)
+
+  const ref = useRef()
+
+  useFrame(() => {
+    ref.current.rotation.x = ref.current.rotation.y += 0.00001
+  })
 
   return (
     <>
       {/* <ambientLight intensity={0.2} />
       <directionalLight /> */}
-      <Sphere args={[4, 32, 32]} >
-        <meshBasicMaterial map={texture} />
+      <Sphere args={[4, 64, 64]} >
+        <meshBasicMaterial map={texture}/>
+      </Sphere>
+      <Sphere ref={ref} args={[4.05, 64, 64]} >
+        <meshBasicMaterial alphaMap={clouds_texture} transparent/>
       </Sphere>
     </>
   )
@@ -40,7 +51,7 @@ function App() {
       {/* <ambientLight /> */}
       {/* <pointLight position={[10, 10, 10]} /> */}
       <Suspense fallback={null}>
-        <Moon position={[2, 2, 2]}/>
+        <Moon position={[4, 4, 4]}/>
         <Earth />
         <OrbitControls/>
       </Suspense>
