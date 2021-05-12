311734a78c66fc0b16501fc60df4c11a76ef43ef
Thom van Dijk <thomvandijk@tutanota.com>
2021-05-12 09:58:04 +0200

    Added earth to the scene

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import { Suspense } from 'react'
 // import { ARCanvas } from '@react-three/xr'
 import { OrbitControls, useTexture, Sphere } from '@react-three/drei'
 
-import moon_texture from './assets/textures/moon.png';
+import moon_texture from './assets/textures/moon.png'
+import earth_texture from './assets/textures/earth.jpeg'
 
-function Moon() {
+function Moon(props) {
   const texture = useTexture(moon_texture)
 
   return (
     <>
       {/* <ambientLight intensity={0.2} />
       <directionalLight /> */}
-      <Sphere args={[1, 32, 32]} >
+      <Sphere {...props} args={[1, 32, 32]} >
+        <meshBasicMaterial map={texture} />
+      </Sphere>
+    </>
+  )
+}
+
+function Earth() {
+  const texture = useTexture(earth_texture)
+
+  return (
+    <>
+      {/* <ambientLight intensity={0.2} />
+      <directionalLight /> */}
+      <Sphere args={[4, 32, 32]} >
         <meshBasicMaterial map={texture} />
       </Sphere>
     </>
@@ -25,9 +40,10 @@ function App() {
       {/* <ambientLight /> */}
       {/* <pointLight position={[10, 10, 10]} /> */}
       <Suspense fallback={null}>
-        <Moon />
+        <Moon position={[2, 2, 2]}/>
+        <Earth />
+        <OrbitControls/>
       </Suspense>
-      <OrbitControls />
     </Canvas>
   );
 }
