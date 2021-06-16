e258b25aeddf189236a0738e5661c0a0efb89c8c
ThomvanDijk <thomvandijk@tutanota.com>
2021-06-16 14:32:02 +0200

    Added different models and animations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { useFrame } from '@react-three/fiber'
+import { useFrame, Canvas } from '@react-three/fiber'
 import { Suspense, useRef } from 'react'
-import { ARCanvas } from '@react-three/xr'
-import { OrbitControls, useTexture, Sphere } from '@react-three/drei'
+// import { ARCanvas } from '@react-three/xr'
+import { OrbitControls, useTexture, Sphere, useGLTF } from '@react-three/drei'
 
 import moon_texture from './assets/textures/moon.png'
 import earth_texture from './assets/textures/earth.jpg'
 import earth_clouds_texture from './assets/textures/earth_clouds.jpg'
+
+import Rocket from './Rocket'
+import Yeti from './Yeti'
+import Ufo from './Ufo'
+import Foguete from './Foguete'
 
 const earth_diameter = 0.4
 
@@ -35,7 +40,7 @@ function Earth(props) {
 
   useFrame(() => {
     // Rotation of clouds with a random unrealistic speed
-    ref.current.rotation.x = ref.current.rotation.y += 0.00001
+    ref.current.rotation.x = ref.current.rotation.y += 0.0002
   })
 
   return (
@@ -50,16 +55,35 @@ function Earth(props) {
   )
 }
 
+function Model(props) {
+  const { nodes, materials } = useGLTF('low_poly_rock/scene.gltf')
+  const texture = useTexture('low_poly_rock/textures/lambert1_baseColor.png')
+
+  return (
+    // <primitive object={gltf.scene} />
+    <mesh geometry={nodes.pCube1_lambert1_0.geometry} material={materials.lambert1} >
+      <meshBasicMaterial attach="material" map={texture} />
+    </mesh>
+  )
+}
+
 function App() {
   return (
-    <ARCanvas>
+    <Canvas style={{height:900}}>
       <Suspense fallback={null}>
+        {/* <pointLight position={[0, 200, 200]} intensity={2} color='#97ffff' /> */}
+        <ambientLight intensity={1} />
+
         {/* The distance between earth and moon is NOT TO SCALE! */}
-        <Moon position={[0, 0, -4]}/>
+        <Moon position={[-1, 0, -1]}/>
         <Earth position={[0, 0, -1]}/>
+
+        <Foguete position={[0, 0, -1]} scale={0.01}/>
+        <Rocket position={[0, 0, -10]}/>
+        <Ufo position={[0, 0, -4]} scale={0.25}/>
         <OrbitControls/>
       </Suspense>
-    </ARCanvas>
+    </Canvas>
   )
 }
 
